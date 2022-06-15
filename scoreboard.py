8824ac943b80a94f006c4e1e7b8c19f3c4f261fa
Milan325 <106485944+Milan325@users.noreply.github.com>
2022-06-15 18:40:59 +0200

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -4,6 +4,7 @@ FONT = ("Courier", 24, "normal")
 
 
 class Scoreboard(Turtle):
+    """creates Scoreboard class using Turtle superclass"""
 
     def __init__(self):
         super().__init__()
@@ -14,13 +15,16 @@ class Scoreboard(Turtle):
         self.update_sb()
 
     def update_sb(self):
+        """clears scoreboard and writes new score"""
         self.clear()
         self.write(f"Level: {self.score}", align="left", font=FONT)
 
     def level_inc(self):
+        """increases score attribute and upadates scoreboard"""
         self.score += 1
         self.update_sb()
 
     def game_over(self):
+        """writes "GAME OVER" at the center of the window"""
         self.goto(0, 0)
         self.write(f"GAME OVER", align="center", font=FONT)
