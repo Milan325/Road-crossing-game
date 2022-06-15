ff8a8175f109bda3829831dc4e5043338480efea
Milan325 <106485944+Milan325@users.noreply.github.com>
2022-06-15 18:44:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,36 +4,40 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-screen.title("TURTLE GAME")
+#### UI and game engine
+if __name__ == "__main__":
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.tracer(0)
+    screen.title("TURTLE GAME")
 
-a_player = Player()
-car_men = CarManager()
-score_board = Scoreboard()
+    a_player = Player()
+    car_men = CarManager()
+    score_board = Scoreboard()
 
-screen.listen()
-screen.onkey(a_player.move, "Up")
+    screen.listen()
+    screen.onkey(a_player.move, "Up")
 
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
+    # game engine
+    game_is_on = True
+    while game_is_on:
+        time.sleep(0.1)
+        screen.update()
 
-    car_men.create()
-    car_men.move_cars()
+        car_men.create()
+        car_men.move_cars()
 
 
-    for car in car_men.all_cars:
-        if car.distance(a_player) < 20:
-            game_is_on = False
+        for car in car_men.all_cars:
+            if car.distance(a_player) < 20:
+                game_is_on = False
 
-    if a_player.ycor() > 280:
-        car_men.speed_increase()
-        a_player.finish()
-        score_board.level_inc()
+        if a_player.ycor() > 280:
+            car_men.speed_increase()
+            a_player.finish()
+            score_board.level_inc()
+    
+    # after breaking from game engine, displaying that game is over
+    score_board.game_over()
 
-score_board.game_over()
-
-screen.exitonclick()
+    screen.exitonclick()
